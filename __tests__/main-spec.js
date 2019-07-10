56ed2aec7cd222ee6b5de3d66d2c3b81b0010b99
JarryLi <3244188372@qq.com>
2019-07-10 20:16:52 +0800

    update test.js about isCartItemsValid case

--- __tests__/main-spec.js
+++ __tests__/main-spec.js
@@ -40,14 +40,23 @@ const database = [
 
 
 it ('should print true when call isEachBarValid given ITEM000000', () => {
+    let barcode = 'ITEM000000';
     expect(Pos_Machine.isEachBarValid(barcode,database)).toBe(true);
 });
 it ('should print false when call isEachBarValid given 000000', () => {
+    let barcode = '000000';
     expect(Pos_Machine.isEachBarValid(barcode,database)).toBe(false);
 });
 it ('should print false when call isEachBarValid given ITEM999999', () => {
+    let barcode = 'ITEM999999';
     expect(Pos_Machine.isEachBarValid(barcode,database)).toBe(false);
 });
 it ('should print true when call isEachBarValid given ITEM000003-2.5', () => {
+    let barcode = 'ITEM000003-2.5';
     expect(Pos_Machine.isEachBarValid(barcode,database)).toBe(true);
+});
+
+it ('should print listInfo when call isCartItemsValid given barcodes', () => {
+    let barcodes = ['ITEM000000','000000','ITEM999999','ITEM000003-2.5'];
+    expect(Pos_Machine.isCartItemsValid(barcodes,database)).toStrictEqual([{itemId:'ITEM000000',valid:true},{itemId:'000000',valid:false},{itemId:'ITEM999999',valid:false},{itemId:'ITEM000003-2.5',valid:true}]);
 });
